4ea4f035757293fa0e8f3fc88ffbf48f5d4ef8e5
Bayarkhuu Tsogtbaatar <tsogtbaatarbayarkhuu@gmail.com>
2026-05-16 14:15:06 +0800

    1

--- server/models/Application.js
+++ server/models/Application.js
@@ -76,17 +76,6 @@ const applicationSchema = new mongoose.Schema(
 );
 
 // Хадгалахын өмнө endDate автоматаар тооцоолох
-applicationSchema.pre("save", function (next) {
-  try {
-    if (this.startDate && this.leaseMonths) {
-      const end = new Date(this.startDate);
-      end.setMonth(end.getMonth() + Number(this.leaseMonths));
-      this.endDate = end;
-    }
-    next();
-  } catch (err) {
-    next(err);
-  }
-});
+
 
 module.exports = mongoose.model("Application", applicationSchema);
